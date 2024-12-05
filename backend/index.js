8eed21033b482c1719831b9a19b536cd8ea8727e
fawazfawu <you@example.com>
2024-12-05 09:57:16 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const RunServer = require('./Database/connection');
 
 
 const app=express();
-const port=process.env.PORT || 3000;
+const port=process.env.PORT;
 
 // json: transmitting the data b/w  client and server
 app.use(express.json())
